cbe4bc4181b9b7ec6012379eb648fdfeb669c951
hakisung <hastareye@gmail.com>
2026-01-28 20:25:14 +0900

    Initial Commit

---
+++ VoiceRecorder/VoiceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceRecorderApp.swift
+//  VoiceRecorder
+//
+//  Created by hakisung on 1/28/26.
+//
+
+import SwiftUI
+
+@main
+struct VoiceRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VoiceRecorderTests/VoiceRecorderTests.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceRecorderTests.swift
+//  VoiceRecorderTests
+//
+//  Created by hakisung on 1/28/26.
+//
+
+import Testing
+@testable import VoiceRecorder
+
+struct VoiceRecorderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
